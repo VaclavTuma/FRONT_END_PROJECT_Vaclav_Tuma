847cfbe5d6129789014b4655e23237b99f197585
Václav Tůma <vaclav@students.zcu.cz>
2024-11-19 00:43:55 +0200

    train classes with expanding picture

--- JAVASC.js
+++ JAVASC.js
@@ -139,7 +139,17 @@ function showSlides(n, slideShowID) {
 }
 
 
-
+function openModal(image) {
+    var modal = document.getElementById("imageModal");
+    var modalImg = document.getElementById("modalImage");
+    modal.style.display = "block";
+    modalImg.src = image.src; // Získání cesty kliknutého obrázku
+  }
+  
+  function closeModal() {
+    var modal = document.getElementById("imageModal");
+    modal.style.display = "none";
+  }
 
 /*
 window.onload = function() {
